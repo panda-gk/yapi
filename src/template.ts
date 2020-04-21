37a5081fd9b3262e181120bf5848f98073749cd0
mockuai <mockuai@mockuaideMacBook-Pro.local>
2020-04-21 15:15:10 +0800

    feat: 修改模版

--- src/template.ts
+++ src/template.ts
@@ -5,11 +5,12 @@ export const configTemplate = `
 const config = {
   target: 'ts',
   serverUrl: 'http://yapi.ywwl.org',
-  outputFilePath: 'api',
+  outputFilePath: 'src/api',
   projectId: '24',
   UM_distinctid: "171637f0112366-06894cf3e07803-15396555-fa000-171637f0113813",
   _yapi_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE4LCJpYXQiOjE1NTY1MDYyMTUsImV4cCI6MTU1NzExMTAxNX0.ADmz2HEE6hKoe1DP_U2QtyKSSEURLf5soGKRNyJkX_o',
   _yapi_uid: '18',
+  generateApiName: (pdth, id) => id
   generateApiFileCode: (api) => {
     const arr = [
       \`
--- yapi.config.ts
+++ yapi.config.ts
@@ -1,20 +1,14 @@
 
-// import { ServerConfig } from 'ywapi2ts'
 
 const config = {
-  target: 'js',
+  target: 'ts',
   serverUrl: 'http://yapi.mockuai.com',
   outputFilePath: 'api',
-  UM_distinctid: '171637f0112366-06894cf3e07803-15396555-fa000-171637f0113813',
   projectId: '1434',
+  UM_distinctid: "171637f0112366-06894cf3e07803-15396555-fa000-171637f0113813",
   _yapi_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjcxNiwiaWF0IjoxNTg3MzQ0Mjg0LCJleHAiOjE1ODc5NDkwODR9.mXDIFHQbX3SAM73Szc_FrSvYCZUlXCQrZH5qt8iKZHk',
   _yapi_uid: '716',
-  // catid: {
-  //   exclude: ['37']
-  // },
-  generateApiName: (path, _id) => {
-    return `api${_id}`
-  },
+  generateApiName: (pdth, id) => `id${id}`,
   generateApiFileCode: (api) => {
     const arr = [
       `
@@ -34,9 +28,9 @@ const config = {
       > => request({
         method: '${api.method}',
         url: '${api.path}',
-        ${(() => {
+        data: ${(() => {
           if (api.method.toLocaleLowerCase() === 'get') {
-            return 'params: data'
+            return '{params: data}'
           } else {
             return 'data'
           }
